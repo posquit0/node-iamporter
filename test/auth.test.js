9247433c8ead240afa0e193052d3ba68aee6c9bd
Posquit0.BJ <posquit0.bj@gmail.com>
2017-01-30 01:46:02 +0900

    Add test cases for API Auth

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,17 +1,39 @@
 'use strict';
 
 const { expect } = require('chai');
-const { Iamporter } = require('../');
+const { Iamporter, IamporterError } = require('../');
 
-describe('Test', function () {
-  it('should success to get a new API token', function (done) {
-    const iamporter = new Iamporter();
-    iamporter.getToken()
-      .then((res) => {
-        expect(res.data).to.have.all.keys([
-          'access_token', 'now', 'expired_at'
-        ]);
-        done();
-      });
+describe('Auth', function () {
+  describe('GET /users/getToken', function () {
+    let iamporter;
+
+    before(function () {
+      iamporter = new Iamporter();
+    });
+
+    it('should success to get a new API token with a default API Key and Secret', function () {
+      return iamporter.getToken()
+        .then((res) => {
+          expect(res.data, 'data').to.have.all.keys([
+            'access_token', 'now', 'expired_at'
+          ]);
+        });
+    });
+
+    it('should fail to get a new API token with invalid API Key and Secret', function () {
+      return iamporter.getToken('InvalidAPIKey', 'InvalidSecret')
+        .catch((err) => {
+          expect(err, 'err').to.be.an.instanceof(IamporterError);
+          expect(err.message, 'err.message').to.equal('아임포트 API 인증에 실패하였습니다.');
+        });
+    });
+
+    it('should fail to get a new API token without API Key and Secret', function () {
+      return iamporter.getToken(null, null)
+        .catch((err) => {
+          expect(err, 'err').to.be.an.instanceof(IamporterError);
+          expect(err.message, 'err.message').to.equal('imp_key, imp_secret 파라메터가 누락되었습니다.');
+        });
+    });
   });
 });
