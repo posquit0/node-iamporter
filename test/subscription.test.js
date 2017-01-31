6bb7c21d173e93485d833c69003f545f6b6f9cea
Posquit0.BJ <posquit0.bj@gmail.com>
2017-02-01 02:50:53 +0900

    Add test cases for subscription

--- test/subscription.test.js
+++ test/subscription.test.js
@@ -4,12 +4,36 @@ const { expect } = require('chai');
 const { Iamporter, IamporterError } = require('../');
 
 describe('Subscription', function () {
-  describe('GET /subscribe/customers/{customer_uid}', function () {
-    let iamporter;
+  let iamporter;
 
-    beforeEach(function () {
-      iamporter = new Iamporter();
+  beforeEach(function () {
+    iamporter = new Iamporter();
+  });
+
+  describe('POST /subscribe/customers/{customer_uid}', function () {
+    it('should success to create a billing key with a private credit card');
+
+    it('should fail when given API token is invalid', function () {
+      iamporter.token = 'invalid-token';
+      iamporter.expireAt = Math.floor(Date.now() / 1000) + 5000;
+
+      const data = {
+        'customer_uid': 'customer_1234',
+        'card_number': '1234-1234-1234-1234',
+        'expiry': '2020-02',
+        'birth': '920220'
+      };
+
+      return iamporter.createSubscription(data)
+        .catch((err) => {
+          expect(err, 'err').to.be.an.instanceof(IamporterError);
+          expect(err.message, 'err.message').to.equal('아임포트 API 인증에 실패하였습니다.');
+        });
     });
+  });
+
+  describe('GET /subscribe/customers/{customer_uid}', function () {
+    it('should success to view a subscription information when billing key is valid');
 
     it('should fail when given API token is invalid', function () {
       iamporter.token = 'invalid-token';
@@ -29,6 +53,28 @@ describe('Subscription', function () {
           expect(err.message, 'err.message').to.match(/등록된 정보를 찾을 수 없습니다./);
         });
     });
+  });
 
+  describe('DELETE /subscribe/customers/{customer_uid}', function () {
+    it('should success to delete a subscription information when billing key is valid');
+
+    it('should fail when given API token is invalid', function () {
+      iamporter.token = 'invalid-token';
+      iamporter.expireAt = Math.floor(Date.now() / 1000) + 5000;
+
+      return iamporter.deleteSubscription()
+        .catch((err) => {
+          expect(err, 'err').to.be.an.instanceof(IamporterError);
+          expect(err.message, 'err.message').to.equal('아임포트 API 인증에 실패하였습니다.');
+        });
+    });
+
+    it('should fail to delete a non-existent billing key', function () {
+      return iamporter.deleteSubscription('iamporter-test-uid')
+        .catch((err) => {
+          expect(err, 'err').to.be.an.instanceof(IamporterError);
+          expect(err.message, 'err.message').to.match(/등록된 정보를 찾을 수 없습니다./);
+        });
+    });
   });
 });
