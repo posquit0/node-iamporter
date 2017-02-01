5184f4feb0480c3a683b847b54d87c256d80b4a3
Posquit0.BJ <posquit0.bj@gmail.com>
2017-02-02 01:08:07 +0900

    Add test cases for payment

--- test/payment.test.js
+++ test/payment.test.js
@@ -48,10 +48,56 @@ describe('Payment', function () {
     });
 
     it('should fail to find a payment information with non-existent uid', function () {
-      return iamporter.findByImpUid('iamporter-test-merchant-uid')
+      return iamporter.findByMerchantUid('iamporter-test-merchant-uid')
         .then((res) => {
           expect(res.message, 'res.message').to.equal('존재하지 않는 결제정보입니다.');
           expect(res.data, 'res.data').to.be.null;
+        });
+    });
+  });
+
+  describe('GET /payments/findAll/{merchant_uid}/{payment_status} - Iamporter.findAllByMerchantUid()', function () {
+    it('should success to find a list of payment informations when uid is valid');
+
+    it('should fail when given API token is invalid', function () {
+      iamporter.token = 'invalid-token';
+      iamporter.expireAt = Math.floor(Date.now() / 1000) + 5000;
+
+      return iamporter.findAllByMerchantUid('iamporter-test-merchant-uid')
+        .catch((err) => {
+          expect(err, 'err').to.be.an.instanceof(IamporterError);
+          expect(err.message, 'err.message').to.equal('아임포트 API 인증에 실패하였습니다.');
+        });
+    });
+
+    it('should fail to find a list of payment informations with non-existent uid', function () {
+      return iamporter.findAllByMerchantUid('iamporter-test-merchant-uid')
+        .then((res) => {
+          expect(res.message, 'res.message').to.equal('존재하지 않는 결제정보입니다.');
+          expect(res.data, 'res.data').to.be.null;
+        });
+    });
+  });
+
+  describe('GET /payments/status/{payment_status} - Iamporter.findAllByStatus()', function () {
+    it('should success to find a list of payment informations when status is valid');
+
+    it('should fail when given API token is invalid', function () {
+      iamporter.token = 'invalid-token';
+      iamporter.expireAt = Math.floor(Date.now() / 1000) + 5000;
+
+      return iamporter.findAllByStatus()
+        .catch((err) => {
+          expect(err, 'err').to.be.an.instanceof(IamporterError);
+          expect(err.message, 'err.message').to.equal('아임포트 API 인증에 실패하였습니다.');
+        });
+    });
+
+    it('should fail to find a list of payment informations with invalid status', function () {
+      return iamporter.findAllByStatus('iamporter-test-status')
+        .catch((err) => {
+          expect(err, 'err').to.be.an.instanceof(IamporterError);
+          expect(err.message, 'err.message').to.equal('지원되지 않는 상태값입니다.');
         });
     });
   });
@@ -91,6 +137,63 @@ describe('Payment', function () {
         .catch((err) => {
           expect(err, 'err').to.be.an.instanceof(IamporterError);
           expect(err.message, 'err.message').to.match(/유효하지않은 카드번호를 입력하셨습니다./);
+        });
+    });
+  });
+
+  describe('POST /subscribe/payments/again - Iamporter.paySubscription()', function () {
+    it('should success to pay with a billing key');
+
+    it('should fail when given API token is invalid', function () {
+      iamporter.token = 'invalid-token';
+      iamporter.expireAt = Math.floor(Date.now() / 1000) + 5000;
+
+      const data = {
+        'customer_uid': 'iamporter-test-customer-uid',
+        'merchant_uid': 'iamporter-test-merchant-uid',
+        'amount': 5000
+      };
+
+      return iamporter.paySubscription(data)
+        .catch((err) => {
+          expect(err, 'err').to.be.an.instanceof(IamporterError);
+          expect(err.message, 'err.message').to.equal('아임포트 API 인증에 실패하였습니다.');
+        });
+    });
+
+    it('should fail to pay with invalid billing key', function () {
+      const data = {
+        'customer_uid': 'iamporter-test-customer-uid',
+        'merchant_uid': 'iamporter-test-merchant-uid',
+        'amount': 5000
+      };
+
+      return iamporter.paySubscription(data)
+        .catch((err) => {
+          expect(err, 'err').to.be.an.instanceof(IamporterError);
+          expect(err.message, 'err.message').to.match(/등록되지 않은 구매자입니다./);
+        });
+    });
+  });
+
+  describe('POST /subscribe/payments/foreign - Iamporter.payForeign()', function () {
+    it('should success to pay with a foreign credit-card');
+
+    it('should fail when given API token is invalid', function () {
+      iamporter.token = 'invalid-token';
+      iamporter.expireAt = Math.floor(Date.now() / 1000) + 5000;
+
+      const data = {
+        'merchant_uid': 'iamporter-test-merchant-uid',
+        'amount': 5000,
+        'card_number': '1234-1234-1234-1234',
+        'expiry': '2020-02'
+      };
+
+      return iamporter.payForeign(data)
+        .catch((err) => {
+          expect(err, 'err').to.be.an.instanceof(IamporterError);
+          expect(err.message, 'err.message').to.equal('아임포트 API 인증에 실패하였습니다.');
         });
     });
   });
