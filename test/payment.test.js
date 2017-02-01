b74755b870626c20ae2d267c93d773a3eb3731cd
Posquit0.BJ <posquit0.bj@gmail.com>
2017-02-01 17:59:48 +0900

    Add test cases for payment onetime

--- test/payment.test.js
+++ test/payment.test.js
@@ -56,6 +56,45 @@ describe('Payment', function () {
     });
   });
 
+  describe('POST /subscribe/payments/onetime - Iamporter.payOnetime()', function () {
+    it('should success to pay onetime with a credit-card');
+
+    it('should fail when given API token is invalid', function () {
+      iamporter.token = 'invalid-token';
+      iamporter.expireAt = Math.floor(Date.now() / 1000) + 5000;
+
+      const data = {
+        'merchant_uid': 'iamporter-test-merchant-uid',
+        'amount': 5000,
+        'card_number': '1234-1234-1234-1234',
+        'expiry': '2020-02',
+        'birth': '920220'
+      };
+
+      return iamporter.payOnetime(data)
+        .catch((err) => {
+          expect(err, 'err').to.be.an.instanceof(IamporterError);
+          expect(err.message, 'err.message').to.equal('아임포트 API 인증에 실패하였습니다.');
+        });
+    });
+
+    it('should fail to pay with invalid credit-card information', function () {
+      const data = {
+        'merchant_uid': 'iamporter-test-merchant-uid',
+        'amount': 5000,
+        'card_number': '1234-1234-1234-1234',
+        'expiry': '2020-02',
+        'birth': '920220'
+      };
+
+      return iamporter.payOnetime(data)
+        .catch((err) => {
+          expect(err, 'err').to.be.an.instanceof(IamporterError);
+          expect(err.message, 'err.message').to.match(/유효하지않은 카드번호를 입력하셨습니다./);
+        });
+    });
+  });
+
   describe('POST /payments/cancel - Iamporter.cancelByImpUid()', function () {
     it('should success to cancel a payment');
 
